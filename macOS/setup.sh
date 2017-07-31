c656a6a3f9f6586a6ce197b077226470905ae2e4
Tyler Wray <wraytw@gmail.com>
2017-07-31 07:48:34 -0600

    Adding more dotfile stuff

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -104,12 +104,6 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightC
 defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -int 1
 defaults -currentHost write com.apple.trackpad.enableSecondaryClick -bool true
 
-# Set a really fast keyboard repeat rate.
-defaults write -g KeyRepeat -int 0
-
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # Set language and text formats. (USD and Imperial Units)
 defaults write -g AppleLanguages -array "en" "nl"
 defaults write -g AppleLocale -string "en_US@currency=USD"
