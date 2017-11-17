879d05775e4d2e292ef1b7cf8539126a3603f5fa
Tyler Wray <wraytw@gmail.com>
2017-11-16 18:56:24 -0700

    Adding Vim

---
+++ vim/init/ctrlp.vim
@@ -0,0 +1,5 @@
+let g:ctrlp_map = '<leader>f'
+let g:ctrlp_max_height = 15
+let g:ctrlp_custom_ignore = {
+  \ 'dir':  'vendor/bundle\|jmeter\|coverage',
+  \ }
---
+++ vim/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
+  echo "Ooops: ~/.vimrc or ~/.vim already exists. \n\
+       It looks like you already have some vim configuration. \n\
+       Try to run ./uninstall.sh"
+else
+  echo === Checking out vundle
+  git submodule update --init
+  cd -
+
+  echo === Linking vim directories
+  ln -s $PWD/vimrc $HOME/.vimrc
+  ln -s $PWD $HOME/.vim
+
+  echo === Vundling
+  vim +PluginInstall +qall
+
+  echo === Done
+fi
