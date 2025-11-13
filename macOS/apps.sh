0a92455019c16b5451837e1359facbbe5e5878b5
Tyler Wray <wraytw@gmail.com>
2025-11-13 10:51:15 -0700

    Cleanup

--- macOS/apps.sh
+++ macOS/apps.sh
@@ -12,34 +12,6 @@ else
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install GUI apps
-gui_apps=(
-  "1password"
-  "alfred"
-  "appcleaner"
-  "daisydisk"
-  "docker"
-  "fantastical"
-  "firefox"
-  "insomnia"
-  "iterm2"
-  "keka"
-  "kindle"
-  "postico"
-  "qlcolorcode"
-  "qlmarkdown"
-  "quicklook-json"
-  "sketch"
-  "slack"
-  "spotify"
-  "visual-studio-code"
-)
-
-for app in "${gui_apps[@]}"
-do
-  brew install --cask $app
-done
-
 # Install Terminal apps
 terminal_apps=(
   "bat"
